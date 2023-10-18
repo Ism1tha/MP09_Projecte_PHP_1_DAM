2e77e356c415b5988ee9977466dd9c884b3791e1
Ism1tha <xgalan@hotmail.com>
2023-10-18 16:48:41 +0200

    Use the connectDB function for the database connection

--- app/index.php
+++ app/index.php
@@ -1,21 +1,10 @@
 <?php 
 
+require 'config.php';
 require 'app/helpers.php';
 require 'app/Task.php';
-require 'config.php';
 
-$driver     = $config['database']['driver'];
-$host       = $user = $config['database']['host'];
-$db_name    = $config['database']['database'];
-$user       = $config['database']['user'];
-$password   = $config['database']['password'];
-
-
-try {
-    $pdo = new PDO("$driver:host=$host;dbname=$db_name", $user, $password);
-} catch (Exception $e) {
-    echo 'Error de connexió a la base de dades: ' . $e;
-}
+$pdo = connectDB($config);
 
 $query = $pdo->prepare("SELECT * from tasks;");
 $query->execute();
